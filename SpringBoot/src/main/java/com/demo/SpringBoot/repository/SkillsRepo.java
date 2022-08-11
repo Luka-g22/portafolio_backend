ad425d414fe5d676e3fef7baee5f06b1046dfa1d
Luca Guerrero <luka222@live.com.ar>
2022-08-11 19:16:59 -0300

    FINAL API

--- SpringBoot/src/main/java/com/demo/SpringBoot/repository/SkillsRepo.java
+++
@@ -1,9 +0,0 @@
-
-package com.demo.SpringBoot.repository;
-
-import com.demo.SpringBoot.model.Skills;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SkillsRepo extends JpaRepository<Skills,Long>{
-    
-}
